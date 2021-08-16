de27f6c32433a3d0fd6d2e9f35843716a0c4bcea
MaXiaoYu <57162041+extheor@users.noreply.github.com>
2021-08-16 16:37:27 +0800

    Add files via upload

---
+++ components.umd.js
@@ -0,0 +1,20 @@
+(function (global, factory) {
+  typeof exports === 'object' && typeof module !== 'undefined' ? module.exports = factory(require('vue')) :
+  typeof define === 'function' && define.amd ? define(['vue'], factory) :
+  (global = typeof globalThis !== 'undefined' ? globalThis : global || self, global.MyComponent = factory(global.Vue));
+}(this, (function (vue) { 'use strict';
+
+  var script = {
+    name: "Tab1"
+  };
+
+  function render(_ctx, _cache, $props, $setup, $data, $options) {
+    return (vue.openBlock(), vue.createElementBlock("div", null, " Tab1 "))
+  }
+
+  script.render = render;
+  script.__file = "src/Tab1/Tab1.vue";
+
+  return script;
+
+})));
